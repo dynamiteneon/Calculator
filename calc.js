98872272078d2d08369622a8adf993e747ae977a
Dan <dan.dobek@gmail.com>
2022-07-20 14:42:12 -0400

    added keypress  functionality, continual operator support

--- calc.js
+++ calc.js
@@ -17,6 +17,7 @@ const clrBtn = document.querySelector('#clear');
 let valueSwitch = false;
 let resultSwitch = false;
 let decSwitch = false;
+let opSwitch = false;
 
 let displayValue;
 
@@ -30,6 +31,55 @@ divBtn.addEventListener('click',setOperator);
 mltplyBtn.addEventListener('click',setOperator);
 addBtn.addEventListener('click',setOperator);
 sbtrctBtn.addEventListener('click',setOperator);
+
+document.addEventListener('keypress', (e)=>
+{
+    let listenNum = e.code;
+    let listenShift = e.shiftKey;
+
+    if(listenShift==true){
+
+        if(listenNum=="Equal"){
+            keyOperator('add');
+        }
+
+        if(listenNum=="Digit8"){
+            keyOperator('multiply');
+        }
+
+    }
+
+    else if(listenShift==false){
+
+    if(listenNum=="Digit0" || listenNum=="Digit1" || listenNum=="Digit2"
+    || listenNum=="Digit3" || listenNum=="Digit4" || listenNum=="Digit5"
+    || listenNum=="Digit6" || listenNum=="Digit7" || listenNum=="Digit8"
+    || listenNum=="Digit9"){
+        
+        let num=listenNum.replace('Digit','');
+        numberKey(num);
+
+    }
+    
+    else if(listenNum=="Period"){
+        addDecimal();
+    }    
+
+
+    else if(listenNum=="Enter"){
+        Calculate();
+    }
+
+    else if(listenNum=="Slash"){
+        keyOperator('divide');
+    }
+
+    else if(listenNum=="Minus"){
+        keyOperator('subtract');
+    }
+}
+});
+
 
 function dispClear(){
 
@@ -48,38 +98,152 @@ function dispClear(){
 function setOperator(e){
     
     let opr = e.target.id;
-    resultSwitch = false;
 
-    if(opr=="divide"){
+    if(opr=="divide" && Operator==""){
         Operator="/";
+        resultSwitch = false;
+        last = Disp.textContent;
+        
+        if(last!=""){
+        lastDisp.textContent = last;
+        }
+        
+        else{}
+
+        Disp.textContent = "0";
+        valueSwitch = true;
+        decSwitch = false;
     }
 
-    else if(opr=="add"){
+    else if(opr=="add" && Operator==""){
         Operator="+";
+        resultSwitch = false;
+        last = Disp.textContent;
+        lastDisp.textContent = last;
+        Disp.textContent = "0";
+        valueSwitch = true;
+        decSwitch = false;
     }
     
-    else if(opr=="subtract"){
+    else if(opr=="subtract" && Operator==""){
         Operator="-";
+        resultSwitch = false;
+        last = Disp.textContent;
+        lastDisp.textContent = last;
+        Disp.textContent = "0";
+        valueSwitch = true;
+        decSwitch = false;
     }
     
-    else if(opr=="multiply"){
+    else if(opr=="multiply" && Operator==""){
         Operator="*";
+        resultSwitch = false;
+        last = Disp.textContent;
+        lastDisp.textContent = last;
+        Disp.textContent = "0";
+        valueSwitch = true;
+        decSwitch = false;
     }    
+
+    else if(opr=="divide" && Operator !=""){
+        Calculate();
+        Operator = "/";
+    }
+
+    else if(opr=="add" && Operator !=""){
+        Calculate();
+        Operator = "+";
+    }
+
+    else if(opr=="subtract" && Operator !=""){
+        Calculate();
+        Operator = "-";
+    }
+
+    else if(opr=="multiply" && Operator !=""){
+        Calculate();
+        Operator = "*";
+    }
 
     else{
 
     }
     
-    console.log(opr);
-    console.log(Operator);
-    
-    last = Disp.textContent;
-    lastDisp.textContent = last;
-    Disp.textContent = "0";
-    
 
-    valueSwitch = true;
-    decSwitch = false;
+}
+
+function keyOperator(opr){
+
+    if(opr=="divide" && Operator==""){
+        Operator="/";
+        resultSwitch = false;
+        last = Disp.textContent;
+        
+        if(last!=""){
+        lastDisp.textContent = last;
+        }
+        
+        else{}
+
+        Disp.textContent = "0";
+        valueSwitch = true;
+        decSwitch = false;
+    }
+
+    else if(opr=="add" && Operator==""){
+        Operator="+";
+        resultSwitch = false;
+        last = Disp.textContent;
+        lastDisp.textContent = last;
+        Disp.textContent = "0";
+        valueSwitch = true;
+        decSwitch = false;
+    }
+    
+    else if(opr=="subtract" && Operator==""){
+        Operator="-";
+        resultSwitch = false;
+        last = Disp.textContent;
+        lastDisp.textContent = last;
+        Disp.textContent = "0";
+        valueSwitch = true;
+        decSwitch = false;
+    }
+    
+    else if(opr=="multiply" && Operator==""){
+        Operator="*";
+        resultSwitch = false;
+        last = Disp.textContent;
+        lastDisp.textContent = last;
+        Disp.textContent = "0";
+        valueSwitch = true;
+        decSwitch = false;
+    }    
+
+    else if(opr=="divide" && Operator !=""){
+        Calculate();
+        Operator = "/";
+    }
+
+    else if(opr=="add" && Operator !=""){
+        Calculate();
+        Operator = "+";
+    }
+
+    else if(opr=="subtract" && Operator !=""){
+        Calculate();
+        Operator = "-";
+    }
+
+    else if(opr=="multiply" && Operator !=""){
+        Calculate();
+        Operator = "*";
+    }
+
+    else{
+
+    }
+    
 
 }
 
@@ -133,8 +297,11 @@ function Calculate(){
             Disp.textContent = Result.toFixed(2);
         }
     }
-
+    if(valueTwo!=""){
     lastDisp.textContent = valueTwo;
+    }
+
+    else{}
     valueTwo = "";
     Operator = "";
     valueOne = Result;
@@ -148,6 +315,7 @@ for(i=0;i<10;i++){
 
     let btn = document.querySelector(`#num${i}`)
     btn.addEventListener('click',numberPress);
+
 
 }
 
@@ -177,7 +345,6 @@ function addDecimal(){
     
 }
 
-    console.log("decimal");
     Disp.textContent = displayValue;
     decSwitch = true;
 
@@ -202,3 +369,20 @@ function numberPress(e){
     Disp.textContent = displayValue;
 
 }
+
+function numberKey(keynum){
+    
+    if(valueSwitch==false && resultSwitch==false){
+        valueOne = valueOne + "" + keynum;
+        displayValue=valueOne;
+    }
+
+    else if(valueSwitch==true){
+        valueTwo = valueTwo + "" + keynum;
+        displayValue=valueTwo;
+    }
+
+    Disp.textContent = displayValue;
+
+}
+
